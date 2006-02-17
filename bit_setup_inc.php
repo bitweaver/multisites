0b85f8231cf4f483e82cda5cf9ca8b670ee413ec
Max Kremmel <xing@synapse.plus.com>
2006-02-17 23:51:44 +0000

    registerPackage now takes a hash instead of multiple parameters - keeps # of parameters under control

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -1,8 +1,13 @@
 <?php
-// $Header: /cvsroot/bitweaver/_bit_multisites/bit_setup_inc.php,v 1.1 2005/06/24 19:56:29 bitweaver Exp $
+// $Header: /cvsroot/bitweaver/_bit_multisites/bit_setup_inc.php,v 1.2 2006/02/17 23:51:44 squareing Exp $
 
 global $gBitSystem, $gMultisites, $gBitLanguage;
-$gBitSystem->registerPackage( 'multisites', dirname( __FILE__ ).'/' );
+
+$registerHash = array(
+	'package_name' => 'multisites',
+	'package_path' => dirname( __FILE__ ).'/',
+);
+$gBitSystem->registerPackage( $registerHash );
 
 if( $gBitSystem->isPackageActive( 'multisites' ) ) {
 	require_once( MULTISITES_PKG_PATH.'Multisites.php' );
