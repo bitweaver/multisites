8ed26e9a7c877f95f20e958074f9957a6bdc2dc8
Max Kremmel <xing@synapse.plus.com>
2006-05-06 15:48:09 +0000

    clean up schema files, remove unused PackageInfo data - we really need a package manager for this, add plugin_path column to liberty_plugins

--- admin/schema_inc.php
+++ admin/schema_inc.php
@@ -1,5 +1,5 @@
 <?php
-// $Header: /cvsroot/bitweaver/_bit_multisites/admin/schema_inc.php,v 1.4 2006/02/19 19:14:22 lsces Exp $
+// $Header: /cvsroot/bitweaver/_bit_multisites/admin/schema_inc.php,v 1.5 2006/05/06 15:48:09 squareing Exp $
 $tables = array(
 	'multisites' => "
 		multisite_id I4 AUTO PRIMARY,
@@ -22,8 +22,5 @@ foreach( array_keys( $tables ) as $tableName ) {
 $gBitInstaller->registerPackageInfo( MULTISITES_PKG_NAME, array(
 	'description' => "Multisites allows you to set up bitweaver for Multi-homing.",
 	'license' => '<a href="http://www.gnu.org/licenses/licenses.html#LGPL">LGPL</a>',
-	'version' => '0.1',
-	'state' => 'alpha',
-	'dependencies' => '',
 ) );
 ?>
